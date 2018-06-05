e50c062791ccc9497ddffa42e1c18629e6de3ec4
Zulux91 <28682730+Zulux91@users.noreply.github.com>
2018-06-05 15:42:30 -0500

    Update AutoB64Decode.js

--- AutoB64Decode.js
+++ AutoB64Decode.js
@@ -5,6 +5,7 @@
 // @version      0.1
 // @description  Autodecodes any Base64 text on a "code box" and show it as a link.
 // @author       Zulux91
+// @updateURL   https://github.com/Zulux91/Base64-Autodecode-Script/blob/master/AutoB64Decode.js
 // @match        none
 // @grant        none
 // ==/UserScript==
@@ -23,8 +24,8 @@ if(document.getElementsByTagName("code")) {
     dale = document.getElementsByTagName("code");
     //Maybe there's more than one, make sure you grab the right one
     for(var i=0;i<dale.length;i++) {
-        //If it looks like a link, it probably is
-        if (dale[i].textContent.indexOf('aHR0') >= 0) {
+        //If it looks like a Base64 link, it probably is
+        if (dale[i].textContent.indexOf('https:') != 0 & dale[i].textContent.indexOf('http:') != 0 & dale[i].textContent.indexOf(' ') != 0) {
             //Put the codebox's text already decoded into this nice variable
             var mylink = b64_to_utf8(dale[i].textContent);
             //Give it some style and show me the money!
